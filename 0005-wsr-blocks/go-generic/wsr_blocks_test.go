a185c646d8f8ab43b24274107a2af865bbeb8805
Jamie Brandon <jamie@scattered-thoughts.net>
2023-10-20 13:41:50 -0700

    Include generics benchmark

--- 0005-wsr-blocks/go-generic/wsr_blocks_test.go
+++ 0005-wsr-blocks/go-generic/wsr_blocks_test.go
@@ -1,6 +1,7 @@
 package wsr_blocks_test
 
 import (
+	"math/rand"
 	"testing"
 	"wsr_blocks"
 
@@ -23,4 +24,54 @@ func TestCompress(t *testing.T) {
 			}
 		}
 	}
+}
+
+type ValueInt int
+
+type Compare[Elem any] interface {
+	IsLess(a Elem) bool
+}
+
+func (a ValueInt) IsLess(b ValueInt) bool {
+	return a < b
+}
+
+func VectorMax(elems []ValueInt) ValueInt {
+	var elemMax = elems[0]
+	for _, elem := range elems {
+		if elemMax.IsLess(elem) {
+			elemMax = elem
+		}
+	}
+	return elemMax
+}
+
+func VectorMax2[Elem Compare[Elem]](elems []Elem) Elem {
+	var elemMax = elems[0]
+	for _, elem := range elems {
+		if elemMax.IsLess(elem) {
+			elemMax = elem
+		}
+	}
+	return elemMax
+}
+
+func DoBenchmark(b *testing.B, f func([]ValueInt) ValueInt) {
+	rand := rand.New(rand.NewSource(42))
+	nums := make([]ValueInt, 1<<16)
+	for i := range nums {
+		nums[i] = ValueInt(rand.Intn(10000))
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = f(nums)
+	}
+}
+
+func BenchmarkVectorMax(b *testing.B) {
+	DoBenchmark(b, VectorMax)
+}
+
+func BenchmarkVectorMax2(b *testing.B) {
+	DoBenchmark(b, VectorMax2[ValueInt])
 }
